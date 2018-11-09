8efb7033aeedabefbe805d5a2a3faf490aa3872a
Vinod Seshadri <vinod.seshadri@hp.com>
2018-11-09 11:20:55 +0530

    first push to repo

---
+++ src/train.py
@@ -0,0 +1,10 @@
+def image_augment(image_source_folder,image_description, image_destination_folder):
+    pass
+def image_search_add_dataset(image_description, image_destination_folder):
+    pass
+def download_base_model(base_model,model_location):
+    pass
+def perform_transfer_model(model_location,image_destination_folder, model_output):
+    pass
+def upload_model():
+    pass
